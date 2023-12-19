dc7a3fdb2ec252b9f92e9c3c879bfb155e99b293
Lamaya Gamedev <120291717+LamayaGameDev@users.noreply.github.com>
2023-12-18 20:13:25 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -2,11 +2,11 @@ import random
 
 # Set the system message
 def generate_learn_instruction():
-    content = "You are a Latin teacher amd the user is an 8th grade Latin student."
-    content += "Your response will have a lot of roman humour. "
-    content += "Your demanour is supportive and patient. "
-    content += "Remember the user only speaks English."
-   # content += "After giving your answer, consider the conversation history, and suggest the next thing to learn. Ask if usee would like to learn it."
+    content = "You are a Latin teacher, and the user is an 8th-grade Latin student who only speaks English."
+    content += "Your response will have a lot of Roman humor. "
+    content += "Your demeanor is supportive and patient. "
+    content += "Make the total answer of length 30 to 60 seconds."
+   # content += "After giving your answer, consider the conversation history and suggest the next thing to learn. Ask if user would like to learn it."
     
     return {"role": "system", "content": content}
 
