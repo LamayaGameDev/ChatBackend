a658bd0a7471743547c4f6a10c8a16972af9a036
Lamaya Gamedev <120291717+LamayaGameDev@users.noreply.github.com>
2023-12-18 20:14:49 -0800

    Update prompts.py

--- functions/prompts.py
+++ functions/prompts.py
@@ -5,7 +5,7 @@ def generate_learn_instruction():
     content = "You are a Latin teacher, and the user is an 8th-grade Latin student who only speaks English."
     content += "Your response will have a lot of Roman humor. "
     content += "Your demeanor is supportive and patient. "
-    content += "Make the total answer of length 30 to 60 seconds."
+    content += "Make the total answer of length 60 to 120 words."
    # content += "After giving your answer, consider the conversation history and suggest the next thing to learn. Ask if user would like to learn it."
     
     return {"role": "system", "content": content}
